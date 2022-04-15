664f7f510390b27db7b293c70238671233df3b69
Masahiro Kaga <masahiro41802891@gmail.com>
2022-04-15 16:56:22 -0700

    Complete number counter

--- section2/app.js
+++ section2/app.js
@@ -1,18 +1,16 @@
 const app = Vue.createApp({
   data() {
     return {
-      name: "Masahiro Kaga",
-      age: 39,
-      agePlusFive: 44,
-      favoriteNumber: 0,
-      favoritePic:
-        "https://imgs.search.brave.com/SSE_QxD9IDG-Sbey3KoNeU1n1R7M6UQuJ_JS_DpefPs/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9nZXR3/YWxscGFwZXJzLmNv/bS93YWxscGFwZXIv/ZnVsbC9lLzMvNC8x/MDAxNDI2LWdvcmdl/cm91cy1wcmV0dHkt/d2ludGVyLWJhY2tn/cm91bmRzLTE5MjB4/MTIwMC5qcGc",
-    };
+        counter:0
+    }
   },
   methods: {
-    favoriteNumberRandom() {
-      return Math.round(Math.random());
+    addCount(){
+        return this.counter++;
     },
+    reduceCount(){
+        return this.counter--;
+    }
   },
 });
 
