fb424baeba45e99e830eb7526a34c22eb4d23403
Masahiro Kaga <masahiro41802891@gmail.com>
2022-04-15 17:47:04 -0700

    Add modifier to any of event.

--- section2/app.js
+++ section2/app.js
@@ -3,9 +3,13 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmName:''
     };
   },
   methods: {
+      confirmInput(event){
+        this.confirmName = event.target.value;
+      },
     submitForm(event) {
     //   event.preventDefault();
       alert('Submitted');
