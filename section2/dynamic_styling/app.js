880bfdcbc6ab75fab0b6d8e934c25542e36a35d8
Masahiro Kaga <masahiro41802891@gmail.com>
2022-04-17 08:50:06 -0700

    Class toggle a bit advanced

--- section2/dynamic_styling/app.js
+++ section2/dynamic_styling/app.js
@@ -1,13 +1,24 @@
 const app = Vue.createApp({
     data(){
         return{
-            firstBoxRed:false
+            boxA:false,
+            boxB:false,
+            boxC:false
         }
     },
     methods:{
-        toggleBoxColor(){
-            this.firstBoxRed = !this.firstBoxRed
-            console.log(this.firstBoxRed)
+        toggleBoxColor(typeofbox){
+            switch(typeofbox){
+                case 'a':
+                    this.boxA = !this.boxA;
+                    console.log(123);
+                    break;
+                case 'b':
+                    this.boxB = !this.boxB;
+                    break;
+                case 'c':
+                    this.boxC = !this.boxC;
+            }
         }
     }
 });
