5482ae0d9cc3f02a4f0993a6e2198a5b079fa260
Masahiro Kaga <masahiro41802891@gmail.com>
2022-04-19 09:59:14 -0700

    [Important] 3 types of getting input value - v-model,@input,ref

--- section5/lesson1/app.js
+++ section5/lesson1/app.js
@@ -1,14 +1,30 @@
 const app1 = Vue.createApp({
     data(){
         return{
-            userTodo:'',
-            todoList:[]
+            userTodo1:'',
+            todoList1:[],
+            userTodo2:'',
+            todoList2:[],
+            userTodo3:'',
+            todoList3:[],
         }
     },
     methods:{
-        addTodo(){
-            this.todoList.push(this.userTodo);
-        }
+        addTodo1(){
+            this.todoList1.push(this.userTodo1);
+        },
+        setText1(e){
+            this.userTodo2 = e.target.value; 
+        },
+        addTodo2(){
+            this.todoList2.push(this.userTodo2);
+        },
+        addTodo3(){
+            this.userTodo3 = this.$refs.usertxt.value; 
+            this.todoList3.push(this.userTodo3);
+            // console.dir(this.$refs.usertxt);
+            // console.log(this.$refs.usertxt);
+        },
     },
     watch:{
 
