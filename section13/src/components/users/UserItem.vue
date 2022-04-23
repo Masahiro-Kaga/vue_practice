e25427656a8d089ed460eb3c281791108f4b1aee
Masa <masahiro41802891@gmail.com>
2022-04-23 10:46:52 -0700

    Applied CSS with condition

--- section13/src/components/users/UserItem.vue
+++ section13/src/components/users/UserItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <li class="m-3 flex flex-col text-center justify-items-center rounded-md border-2 max-w-[300px] mx-auto">
+    <li class="m-3 flex flex-col text-center justify-items-center rounded-md border-2 max-w-[300px] mx-auto" :class="roleClass">
         <h3>{{name}}</h3>
         <div>{{role}}</div>
     </li>
@@ -7,6 +7,16 @@
 
 <script>
     export default {
-        props:['name','role']
+        props:['name','role'],
+        computed:{
+            roleClass(){
+                if(this.role === 'Engineer'){
+                    return 'bg-[red]'
+                }
+                if(this.role === 'Consultant'){
+                    return 'bg-[blue]'
+                }
+            }
+        }
     }
 </script>
