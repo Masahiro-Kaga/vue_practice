5059ae34c0c91f4172a1aeaea58f894c928e63f4
Masa <masahiro41802891@gmail.com>
2022-04-23 10:17:39 -0700

    Set provide and inject on Users component

--- section13/src/components/users/UsersList.vue
+++ section13/src/components/users/UsersList.vue
@@ -1,12 +1,20 @@
 <template>
-    <div>
-World
-    </div>
+    <li @click="test" class="list-none">
+
+    </li>
 </template>
 
 <script>
     export default {
-        
+        inject:['users'],
+        methods:{
+            test(){
+                console.log(this.users)
+            }
+        },
+        computed:{
+
+        }
     }
 </script>
 
